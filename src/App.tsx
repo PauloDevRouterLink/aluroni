0ed4f8208e08820f7aab6241150351d51d7e94c9
PauloWeslley13 <paulo.firmino@routerlink.com.br>
2023-11-17 14:51:13 -0300

    chore: config project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+	return (
+		<main>
+			<section>
+				<h1>Hello World</h1>
+				<span>Hello</span>
+				<span>Aluroni</span>
+			</section>
+		</main>
+	)
+}
---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export const Menu = () => {
+	return (
+		<main>
+			<nav className={styles.menu}>Aluroni</nav>
+		</main>
+	)
+}
